b34b545b188806ffda8cab7985edbc165da81e25
nrocio <nancy.perez.contreras469@gmail.com>
2022-08-11 03:20:07 +0000

    Added challenge

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -9,3 +10,7 @@ async def root():
 @app.get("/myname/{name}")
 async def myName(name: str):
     return {"message": f"Hello {name} this is my new API!"}
+
+@app.get("/technology")
+async def redirect_typer():
+    return RedirectResponse("https://images.pexels.com/photos/3978352/pexels-photo-3978352.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
